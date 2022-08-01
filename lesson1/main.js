6ef07bea83a6c1c7dc4673b22ada563412d0f7e6
Ivan77727 <vanya.mdfluxgmail.com>
2022-08-01 20:20:10 +0300

    add lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,95 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+let hello = 'hello';
+console.log(hello);
+document.write(`${hello} `);
+alert(hello);
+
+let owu = 'owu';
+console.log(owu);
+document.write(`${owu} `);
+alert(owu);
+
+let com = 'com';
+console.log(com);
+document.write(`${com} `);
+alert(com);
+
+let ua= 'ua';
+console.log(ua);
+document.write(`${ua} `);
+alert(ua);
+
+let number = 1;
+console.log(number);
+document.write(`${number} `);
+alert(number);
+
+let number2 = 10;
+console.log(number2);
+document.write(`${number2} `);
+alert(number2 );
+
+let number3 = -999;
+console.log(number3);
+document.write(`${number3} `);
+alert(number3);
+
+let number4 = 123;
+console.log(number4);
+document.write(`${number4} `);
+alert(number4);
+
+let number5 = 3.14;
+console.log(number5);
+document.write(`${number5} `);
+alert(number5);
+
+let number6 = 2.7;
+console.log(number6);
+document.write(`${number6} `);
+alert(number6);
+
+let number7 = 16;
+console.log(number7);
+document.write(`${number7} `);
+alert(number7);
+
+let boolean = true;
+console.log(boolean);
+document.write(`${boolean}`);
+alert(boolean);
+
+let boolean2 = false;
+console.log(boolean2);
+document.write(`${boolean2}`);
+alert(boolean2);
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person
+// (Не об'єкт, просто за допомоги конкатенації)
+
+// let firstname = `Ivan`;
+// let middleName = `Andriyovych`;
+// let lastName = `Kinash`;
+// let res = `${firstname} ${middleName} ${lastName} `
+// console.log(res);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// та вивести в консоль
+
+// let name=prompt(`Enter name`);
+// let middleName =prompt( `Enter middleName`);
+// let age = +prompt(`Enter age`)
+// console.log(name);
+// console.log(middleName);
+// console.log(age);
